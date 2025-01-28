b9aa5ac457f879e90004be154328400405bc01b2
Nick Kapsiak <nkapsiak@outlook.com>
2025-01-28 16:10:51 -0500

    fixed dr login screen

--- lib/screens/login screen/login_doctor_screen.dart
+++ lib/screens/login screen/login_doctor_screen.dart
@@ -1,5 +1,6 @@
 // **Login Screen for Doctors**
 import 'dart:convert';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart'as http;
 import 'package:flutter/material.dart';
 
@@ -86,38 +87,111 @@ class _LoginDoctorScreenState extends State<LoginDoctorScreen> {
   }
 
   @override
+  bool passToggle = true;
   Widget build(BuildContext context) {
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Doctor Login'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(labelText: 'Username'),
+      appBar: AppBar(title: Text("Doctor Login"),),
+      body: Material(
+        child: Container(
+          color: Colors.white,
+          child: SingleChildScrollView(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  SizedBox( height: 10,),
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Image.asset("images/doctors.jpg"),
+                  ),
+                  SizedBox(height: 10,),
+                  Padding(padding: EdgeInsets.all(12),
+                    child: TextField(
+                        controller: _usernameController,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            label: Text("Enter Username"),
+                            prefixIcon: Icon(Icons.person)
+                        )
+                    ),
+                  ),
+                  Padding(padding: EdgeInsets.all(12),
+                    child: TextField(
+                        controller: _passwordController,
+                        obscureText: passToggle ? true : false,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            label: Text("Enter Password"),
+                            prefixIcon: Icon(Icons.lock),
+                            suffixIcon: InkWell(
+                              onTap: (){
+                                if(passToggle == true){
+                                  passToggle = false;
+                                }
+                                else{
+                                  passToggle = true;
+                                }
+                                setState(() {
+
+                                });
+                              },
+                              child: passToggle
+                                  ? Icon(CupertinoIcons.eye_slash_fill)
+                                  : Icon(CupertinoIcons.eye_slash),
+                            )
+                        )
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Material(
+                        color: Color(0xFF700031),
+                        borderRadius: BorderRadius.circular(10),
+                        child: InkWell(
+                          onTap: _loginDoctor,
+                          child: Padding(padding:
+                          EdgeInsets.symmetric(vertical: 15, horizontal: 40 ),
+                            child: Center(
+                              child: Text("Log In",
+                                style: TextStyle(color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold),),
+                            ),),
+
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text("Don't have an account?",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black54,
+                        ),),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, '/doctorRegister');
+                          },
+                          child: Text("Create Account",
+                            style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xFF700031)
+                            ),))
+                    ],
+                  )
+
+                ],
+              ),
             ),
-            TextField(
-              controller: _passwordController,
-              decoration: InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            _isLoading
-                ? CircularProgressIndicator()
-                : ElevatedButton(
-              onPressed: _loginDoctor,
-              child: Text('Login'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/doctorRegister');
-              },
-              child: Text('Don\'t have an account? Register as Doctor'),
-            ),
-          ],
+          ),
         ),
       ),
     );
